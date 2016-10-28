6073b9987f950f9d4316394fe8d5b87b9b72b558
Rubel Sarker <beingrubel@gmail.com>
2016-10-28 19:38:20 +0100

    f

---
+++ src/com/rubel/app/model/User.java
@@ -0,0 +1,30 @@
+package com.rubel.app.model;
+
+
+public class User {
+
+    private String userName;
+    private String email;
+
+    public User(String userName, String email, String password, String dob, int credit) {
+        super();
+        this.userName = userName;
+        this.email = email;
+
+    }
+    public User() {
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
